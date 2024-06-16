754924eda00fedd7578f1b2b482c636292f6ab36
Anderson Allen <daa729@gmail.com>
2024-06-16 18:12:06 -0400

    try try again

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
     output:'export',
-    images: { unoptimized: true}
-    };
+    images: { unoptimized: true},
+    env:{
+        NEXT_PUBLIC_API_KEY: process.env.NEXT_PUBLIC_API_KEY,
+    }
+
+
+};
 
 export default nextConfig;
